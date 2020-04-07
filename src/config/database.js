d88af82e2b9286a0d58457060297b4790ab883a2
radaelilucca <radaelidev@gmail.com>
2020-04-06 22:43:31 -0300

    Sequelize Configs

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ module.exports = {
   port: "5432",
   database: process.env.DB_NAME,
   production: {
-    use_env_variable: process.env.DB_URL,
+    use_env_variable: process.env.NODE_ENV,
   },
   define: {
     tiestamps: true,
