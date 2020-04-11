8c694c0fef4b930a78efc1c4258904ecf7ba79e5
radaelilucca <radaelidev@gmail.com>
2020-04-11 16:35:51 -0300

    Disable auth middleware

--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,41 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 // import Controllers
 
-import OngController from './app/controllers/OngController';
-import SessionController from './app/controllers/SessionController';
-import IncidentController from './app/controllers/IncidentController';
-import ProfileController from './app/controllers/ProfileController';
+import OngController from "./app/controllers/OngController";
+import SessionController from "./app/controllers/SessionController";
+import IncidentController from "./app/controllers/IncidentController";
+import ProfileController from "./app/controllers/ProfileController";
 
-import authMiddleware from './app/middlewares/auth';
+import authMiddleware from "./app/middlewares/auth";
 
 const routes = Router();
 
 // Create Ong
 
-routes.post('/ongs', OngController.store);
+routes.post("/ongs", OngController.store);
 
 // Create session
-routes.post('/sessions/:id', SessionController.store);
+routes.post("/sessions/:id", SessionController.store);
 
 // List all incidents
-routes.get('/incidents/', IncidentController.index);
+routes.get("/incidents/", IncidentController.index);
 
 // List all ongs
-routes.get('/ongs', OngController.index);
+routes.get("/ongs", OngController.index);
 
 // ------------------- Login required routes ---------------------
-routes.use(authMiddleware);
+//routes.use(authMiddleware);
 
-routes.delete('/incidents/delete/:incident_id', IncidentController.delete);
+routes.delete("/incidents/delete/:incident_id", IncidentController.delete);
 
 // Create incident
-routes.post('/incidents/create', IncidentController.store);
+routes.post("/incidents/create", IncidentController.store);
 
 // List incidents of logged ong
-routes.get('/profile', ProfileController.index);
+routes.get("/profile", ProfileController.index);
 
 // update incident
-routes.put('/incidents/:id', IncidentController.update);
+routes.put("/incidents/:id", IncidentController.update);
 
 export default routes;
