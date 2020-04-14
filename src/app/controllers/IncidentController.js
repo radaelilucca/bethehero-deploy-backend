51b0c0ff124fbc7086f10020f8a719fd562e4bc2
radaelilucca <radaelidev@gmail.com>
2020-04-14 19:26:31 -0300

    Fix - Limit and order - incidents index

--- src/app/controllers/IncidentController.js
+++ src/app/controllers/IncidentController.js
@@ -85,6 +85,9 @@ class IncidentController {
   async index(req, res) {
     const { page = 1 } = req.query;
     const incidents = await Incident.findAndCountAll({
+      limit: 4,
+      offset: (page - 1) * 4,
+      order: ["created_at"],
       include: [
         {
           model: Ong,
